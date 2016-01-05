caf311e6db7d317f4bbd3f633c3ab8f22fcbd489
Jacob G Scott <jacob.g.scott@gmail.com>
2016-01-05 16:56:43 -0500

    multiBx_single_plot.py is currently a mess, heavy experimentation to follow

--- multiBx_single_plot.py
+++ multiBx_single_plot.py
@@ -8,6 +8,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import pylab as P
 from scipy.spatial import distance
 # from collections import Counter
 from math import log as ln
@@ -56,11 +57,11 @@ def do_biopsies(size, biopsy_num, r, CM1, biopsy_sites):
 size = 100 #size of the array
 time = 100
 #pick X random points, then find all the other elements within a range, r, of the point
-biopsy_num = 30 #desired number of biopsies
+biopsy_num = 20 #desired number of biopsies
 SI1 = 0 #placeholders for Shannon Index values
 total_mut1 = np.zeros(size**2) #placeholders for mutation arrays
 SIBx = [] #list of shannon indices for each biopsy
-r_sweep = 5
+r_sweep = 3
 
 read_path = '../andrea_test/non-stem/text/'
 write_path = '../figs/multi_bx/'
@@ -75,7 +76,7 @@ N1 = np.count_nonzero(CA)
 for species in range (1, np.amax(CA)): SI1 = SI1 + shannon(np.bincount(CA)[species],N1)
 SItrunc = float("{0:.4f}".format(SI1))
 
-################ INITIALIZE COUNTERS #################
+''' INITIALIZE COUNTERS '''
 
 #'biopsy' at random some circle of cells
 biopsy_sites = [] #a list of the sites of biopsy - ordered pairs
@@ -83,6 +84,7 @@ biopsied_cells = [] #a list of lists of biopsied cells
 hist_list = [] #a list of histogram data
 
 ### PLOT
+
 rcParams['figure.figsize'] = 8,8
 z = np.zeros((r_sweep,biopsy_num))
 for i in range(1,r_sweep+1):
@@ -91,39 +93,61 @@ for i in range(1,r_sweep+1):
 	biopsy_sites = gather_biopsies(biopsy_num,2*i)
 	SIBx = do_biopsies(size, biopsy_num, 2*i, CM1, biopsy_sites)
 	hist_list.append(SIBx)
+data=[]
+bins = np.linspace(0, np.max(hist_list), 100)
+# for i in (np.arange(r_sweep)):
+# 	data.append(hist_list[i])
+# # data.append(hist_list[i] for i in (np.arange(r_sweep)))
+# print data
+# print hist_list[0]
+# n, bins = P.hist([hist_list[0,:], hist_list[:,1], hist_list[:,2] ], 10, histtype='bar')
 
+
+
+'''  plots separate plots for each histogram
+for i in (np.arange(r_sweep)):
+	plt.figure()
+	plt.hist(hist_list[i], bins, alpha = 0.5, label = str(i))
+
+'''
+
+#makes a nice, but unreadable multicolored bar chart
 x = np.linspace(1,r_sweep,r_sweep)
 y = np.linspace(1,biopsy_num,biopsy_num)
 x,y = np.meshgrid(x,y)
-print x,y
+
+colors = ['r','g','b','k']
+ci = 0
+
 for i in range(0,r_sweep): 
 	for j in range(0,biopsy_num): 
 		z[i][j] = hist_list[i][j]
 Z = np.reshape(z, (biopsy_num,r_sweep))
+# print Z[:,1]
+xs = np.arange(biopsy_num)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+for y in (np.arange(r_sweep)):
+	z = Z[:,y]
+	ci += 1
+	cz = ci%len(colors)
+	ax.bar(xs, z, zs=y, zdir='y', alpha=0.8, color = colors[cz])
+
+ax.set_xlabel('biopsies')
+# ax.xticks(x, np.arange(biopsy_num))
+ax.set_ylabel('radius of biopsy')
+ax.set_zlabel('Shannon Index')
+
 
 # plt.subplot(1,2,1)
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-surf = ax.plot_surface(x,y,Z, rstride=1, cstride=1, cmap='nipy_spectral', linewidth=0, antialiased=False)
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+# surf = ax.plot_surface(x,y,Z, rstride=1, cstride=1, cmap='nipy_spectral', linewidth=0, antialiased=False)
+# fig.colorbar(surf, shrink=0.5, aspect=5)
 # plt.surf(SIBx)
 # plt.xlabel('Shannon Index')	
 # plt.ylabel('frequency')
 # plt.title('S.I.s of r = '+str(r1)+ 'bx \n mean:'+str(meanBx1)+' std:'+str(stdBx1))
 
-
-rcParams['figure.figsize'] = 5,5
-# plt.subplot(1,2,2)
-plt.figure()
-ax1 = plt.pcolor(CM1, cmap='nipy_spectral', vmin = 0.001)
-# plt.colorbar()
-plt.title('Tumor size: '+str(N1)+' cells')
-# x,y = zip(*biopsy_sites)
-# # loop through all triplets of x-,y-coordinates and radius and plot a circle for each:
-# for x, y in zip(x, y):
-#     plt.gca().add_artist(Circle(xy=(x, y), radius=r1, alpha = 1, fill = False, color = 'w'))
-# plt.xlim([0, size])
-# plt.ylim([0, size])
-
-
-# plt.savefig(write_path+'NHist'+str(biopsy_num)+'Bx_r:'+str(r1)+'thru:'+str(r2)+'.png', dpi = 500)
+# plt.savefig(write_path+'NHist'+str(biopsy_num)+'Bx_r=2'+'thru_'+str(2*r_sweep)+'.png', dpi = 500)
 plt.show()
